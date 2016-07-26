07737fd543e97b506c427af2b27cc6e8172739f2
Hazar Snoussi <hazar.snoussi@gmail.com>
2016-07-26 13:15:18 -0400

    upper/lower case f error
    MainJFrame instead of MainJframe

--- src/cheers_pb/MainJFrame.java
+++ src/cheers_pb/MainJFrame.java
@@ -6,9 +6,9 @@ package cheers_pb;
 
 import mathematical_Functions.OverlapDistance;
 
-public class MainJframe extends javax.swing.JFrame {
+public class MainJFrame extends javax.swing.JFrame {
 
-    public MainJframe() {
+    public MainJFrame() {
         initComponents();
     }
 
@@ -195,18 +195,18 @@ public class MainJframe extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainJframe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainJframe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainJframe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainJframe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
 
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new MainJframe().setVisible(true);
+                new MainJFrame().setVisible(true);
             }
         });
     }
